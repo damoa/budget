a54733b68db0086504cff7b85a7731c12fddca4c
Damoa Obiri Alcorta <damoa.alcorta@sofatutor.com>
2014-07-12 18:30:55 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Haushalt::Application.config.secret_token = '59875c075e0f22db2e50df51aae8368b38828df5bc81971ba96fd2026adc4abc848049b3de25d3b399d1a2a098ffe0e67b9321ee46002803127b595b955b8188'
