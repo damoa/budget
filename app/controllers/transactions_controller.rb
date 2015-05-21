477d046cbd3e3d55d440974c3daeded720acf1e2
Damoa Obiri <damoa@gmx.com>
2015-05-21 11:46:51 +0200

    Remove trailling white spaces

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -11,7 +11,7 @@ class TransactionsController < ApplicationController
       format.json {render :json => @transactions.map { |t| [t.value_date_in_milliseconds, t.amount, t.balance]}}
     end
   end
-  
+
   def monthly
     @transactions = Transaction.monthly_sum.all
     respond_to do |format|
@@ -19,7 +19,7 @@ class TransactionsController < ApplicationController
       format.json {render :json => @transactions.map { |t| [t.value_date, t.amount]}}
     end
   end
-  
+
   def monthly_withdrawal
     @transactions = Transactions::Withdrawal.monthly_sum.all
     respond_to do |format|
@@ -27,7 +27,7 @@ class TransactionsController < ApplicationController
       format.json {render :json => @transactions.map { |t| [t.value_date, t.amount]}}
     end
   end
-  
+
   def overall
     @transactions = Transaction.by_type.all
     respond_to do |format|
